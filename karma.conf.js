63895da0dfe50f107c571ec2d698a466e81d3042
Rob Macci <Rob Macci>
2017-02-05 10:21:47 -0500

    buildFixes

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,8 @@
 module.exports = function(config) {
 
-  var appBase    = 'src/';       // transpiled app JS and map files
-  var appSrcBase = appBase;      // app source TS files
+  var appBase    = 'app/';       // transpiled app JS and map files
+  var appSrcBase = 'app/';       // app source TS files
+  var appAssets  = '/base/app/'; // component assets fetched by Angular's compiler
 
   // Testing helpers (optional) are conventionally in a folder called `testing`
   var testingBase    = 'testing/'; // transpiled test JS and map files
@@ -56,8 +57,8 @@ module.exports = function(config) {
       { pattern: 'node_modules/@angular/**/*.js', included: false, watched: false },
       { pattern: 'node_modules/@angular/**/*.js.map', included: false, watched: false },
 
-      { pattern: appBase + '/systemjs.config.js', included: false, watched: false },
-      { pattern: appBase + '/systemjs.config.extras.js', included: false, watched: false },
+      { pattern: 'systemjs.config.js', included: false, watched: false },
+      { pattern: 'systemjs.config.extras.js', included: false, watched: false },
       'karma-test-shim.js', // optionally extend SystemJS mapping e.g., with barrels
 
       // transpiled application & spec code paths loaded via module imports
@@ -71,7 +72,7 @@ module.exports = function(config) {
       { pattern: appBase + '**/*.css', included: false, watched: true },
 
       // Paths for debugging with source maps in dev tools
-      { pattern: appBase + '**/*.ts', included: false, watched: false },
+      { pattern: appSrcBase + '**/*.ts', included: false, watched: false },
       { pattern: appBase + '**/*.js.map', included: false, watched: false },
       { pattern: testingSrcBase + '**/*.ts', included: false, watched: false },
       { pattern: testingBase + '**/*.js.map', included: false, watched: false}
@@ -79,8 +80,8 @@ module.exports = function(config) {
 
     // Proxied base paths for loading assets
     proxies: {
-      // required for modules fetched by SystemJS
-      '/base/src/node_modules/': '/base/node_modules/'
+      // required for component assets fetched by Angular's compiler
+      "/app/": appAssets
     },
 
     exclude: [],
